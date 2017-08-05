fdf77e366bfe6560b00b13aa1caae94901c1d48c
saranraju <saranraju@yahoo.com>
2017-08-05 14:18:39 -0700

    setp PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'nodeMailCampaignTool',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-54-89-49-145.compute-1.amazonaws.com',
+      key: 'E:/AWS/saran.pem',
+      ref: 'origin/master',
+      repo: 'https://github.com/saranraju/nodeMailCampaignTool.git',
+      path: '/home/ubuntu/nodeMailCampaignTool',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
